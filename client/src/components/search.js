bc9e3503ae3745321283fdfc6d710cf2f2989615
Falon Darville <falondarville@gmail.com>
2018-06-16 21:45:39 -0700

    fix proxy and test handleSubmit

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -21,13 +21,14 @@ export default class Search extends Component {
 
 	handleSubmit = (event) => {
 		// send data to backend, which will process the NYT API
+		console.log("handleSubmit is working")
 		event.preventDefault();
 
 		const { topic, startYear, endYear } = this.state;
 
 		// pass variables to backend to complete the API request
 		// this is not communicating yet
-		axios.get('/api/articles', {
+		axios.get('http://localhost:3000/api/articles', {
 			topic, startYear, endYear
 		})
 		.then(function(data){
@@ -46,18 +47,18 @@ export default class Search extends Component {
 				  <div className="card-header text-center">
 				    <h3>Search</h3>
 				  </div>
-				  <div className="card-body">
-				    <h4 className="card-text text-center">Topic</h4>
-				    <input className="form-control form-control-lg" onChange={this.handleChange} name="topic" type="text" placeholder="Enter your search topic" />
+					  <div className="card-body">
+					    <h4 className="card-text text-center">Topic</h4>
+					    <input className="form-control form-control-lg" onChange={this.handleChange} name="topic" type="text" placeholder="Enter your search topic" />
 
-				    <h4 className="card-text text-center">Start Year</h4>
-				    <input className="form-control form-control-lg" onChange={this.handleChange} name="start-year" type="text" placeholder="Enter the start year for your query" />
+					    <h4 className="card-text text-center">Start Year</h4>
+					    <input className="form-control form-control-lg" onChange={this.handleChange} name="start-year" type="text" placeholder="Enter the start year for your query" />
 
-				    <h4 className="card-text text-center">End Year</h4>
-				    <input className="form-control form-control-lg" onChange={this.handleChange} name="end-year" type="text" placeholder="Enter the end year for your query" />
+					    <h4 className="card-text text-center">End Year</h4>
+					    <input className="form-control form-control-lg" onChange={this.handleChange} name="end-year" type="text" placeholder="Enter the end year for your query" />
 
-				    <a onSubmit={this.handleSubmit} className="btn search-btn btn-primary float-right">Search</a>
-				  </div>
+					    <a onClick={this.handleSubmit} className="btn search-btn btn-primary float-right">Search</a>
+					  </div>
 				</div>
 			</div>
 			)
