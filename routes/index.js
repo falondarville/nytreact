a8a3497e7148c4648db641e16bc2fad4bedc98c6
Falon Darville <falondarville@gmail.com>
2018-06-21 12:02:13 -0700

    add * route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
+var path = require('path');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/*', function(req, res, next) {
+ 	res.sendFile(path.join(__dirname + '/../client/build/index.html'));
 });
 
 module.exports = router;
